ed3624f6fc9597401be62f1f5a51d60c729d1ba8
CarlosIS2 <cestebanleon@gmail.com>
2018-02-25 09:41:15 +0000

    Menú embebido en php para usarlo en todas las páginas

---
+++ src/Main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Main</title>
+    <link rel="stylesheet" href="Styles/Style.css">
+</head>
+<body>
+    <div>
+        <?php include_once("Menu.html") ?>
+    </div>
+    <div>
+        <h1>ProFootballDraft</h1>
+    </div>
+</body>
+</html>
